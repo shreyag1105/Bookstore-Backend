fbfdd75c0e398158752105339b553ece640db101
Shreya Gupta <shreya.g1105@gmail.com>
2025-04-09 02:35:48 +0530

    BookController.java

--- src/main/java/xyz/seanchao/bookstore/controller/BookController.java
+++ src/main/java/xyz/seanchao/bookstore/controller/BookController.java
@@ -1,37 +1,22 @@
-package xyz.seanchao.bookstore.controller;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.web.bind.annotation.RequestParam;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
-import xyz.seanchao.bookstore.entity.Book;
-import xyz.seanchao.bookstore.service.BookService;
+// ...
 
-import java.util.ArrayList;
-import java.util.List;
+@GetMapping("/books")
+public Page<Book> getAllBooks(
+        @RequestParam(defaultValue = "0") int page,
+        @RequestParam(defaultValue = "10") int size,
+        @RequestParam(defaultValue = "id") String sortBy,
+        @RequestParam(defaultValue = "asc") String direction
+) {
+    Sort sort = direction.equalsIgnoreCase("desc")
+            ? Sort.by(sortBy).descending()
+            : Sort.by(sortBy).ascending();
 
-@RestController
-public class BookController {
-
-    @Autowired
-    private BookService bookService;
-
-    @GetMapping(value = "/book/{id}")
-    public Book findBook(@PathVariable("id") Integer id) {
-        System.out.println("Searching book id: " + id);
-        Book book = bookService.findBookById(id);
-        System.out.println(book.getId() + " " + book.getName());
-        return book;
-    }
-
-    @GetMapping(value = "/getBookAll")
-    public List<Book> getBookAll() {
-        return bookService.findAll();
-    }
-
-    @GetMapping(value = "/author/{name}")
-    public List<Book> findByAuthor(@PathVariable("name") String author) {
-        System.out.println("find books by author: " + author);
-        return bookService.findByAuthor(author);
-    }
+    Pageable pageable = PageRequest.of(page, size, sort);
+    return bookService.findAllPaginated(pageable);
 }
